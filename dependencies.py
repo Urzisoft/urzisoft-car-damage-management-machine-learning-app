153ef8e932a9ab07f48b4e4d613fb3f40b3b46b7
ciganoliviu <ciganoliviudavid@gmail.com>
2023-10-14 15:54:07 +0300

    Updates 2x

--- dependencies.py
+++ dependencies.py
@@ -2,13 +2,12 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 
-
 STANDARD_DATA_DIRECTORY_STRUCTURE = 'dataset/validation/'
 STANDARD_RESIZE_VALUE = 300
 classifier_classes = {'minor': 0, 'moderate': 1, 'severe': 2}
 
 
-def collect_images_from_files(X, Y, classifier_classes, resize_value = STANDARD_RESIZE_VALUE):
+def collect_images_from_files(X, Y, classifier_classes, resize_value=STANDARD_RESIZE_VALUE):
     for cls_class in classifier_classes:
         path = STANDARD_DATA_DIRECTORY_STRUCTURE + cls_class
         for file in os.listdir(path):
@@ -16,14 +15,12 @@ def collect_images_from_files(X, Y, classifier_classes, resize_value = STANDARD_
             img = cv2.resize(img, (resize_value, resize_value))
             X.append(img)
             Y.append(classifier_classes[cls_class])
-            
-            
-def test_based_on_images(classifier, file_testing_class, size = 9, resize_value = STANDARD_RESIZE_VALUE):
+
+
+def test_based_on_images(classifier, file_testing_class, size=9, resize_value=STANDARD_RESIZE_VALUE):
     values_classifier = {0: 'minor', 1: 'moderate', 2: 'severe'}
-    
+
     plt.figure(figsize=(12, 8))
-    path = os.listdir(STANDARD_DATA_DIRECTORY_STRUCTURE)
-    
     subplot_index = 1
 
     for i in os.listdir(STANDARD_DATA_DIRECTORY_STRUCTURE + file_testing_class)[:size]:
@@ -34,9 +31,9 @@ def test_based_on_images(classifier, file_testing_class, size = 9, resize_value 
         img_resized = img_resized.reshape(1, -1) / 255
         path = classifier.predict(img_resized)
         plt.title(values_classifier[path[0]])
-        plt.imshow(img, cmap = 'gray')
+        plt.imshow(img, cmap='gray')
         plt.axis('off')
-        
+
         if subplot_index == 9:
             subplot_index = 1
         else:
@@ -46,7 +43,7 @@ def test_based_on_images(classifier, file_testing_class, size = 9, resize_value 
 def scale_data_tests(x_train, x_test):
     x_train = x_train / 255
     x_test = x_test / 255
-    
+
     return x_train, x_test
 
 
